2ddb5751db15fb613a655edb734c50283e122f55
Mino <k3000js@naver.com>
2021-03-22 18:11:37 +0900

    백준 - N11948

---
+++ src/baekjoon/N11948.kt
@@ -0,0 +1,18 @@
+package baekjoon
+
+import kotlin.math.max
+
+fun main(){
+    val list = mutableListOf<Int>()
+    repeat(6) {
+        list.add(readLine()!!.toInt())
+    }
+    var a = 0
+    for((index, num) in list.subList(0,4).sorted().withIndex()){
+        if(index != 0)
+            a += num
+    }
+    val b = max(list[4],list[5])
+
+    println(a+b)
+}
