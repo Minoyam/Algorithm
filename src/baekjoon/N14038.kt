011064cd4af30aeb563a153f51f7f2693056997d
Mino <k3000js@naver.com>
2021-03-25 10:22:59 +0900

    백준 -N14038

---
+++ src/baekjoon/N14038.kt
@@ -0,0 +1,24 @@
+package baekjoon
+
+fun main() {
+    var count = 0
+    repeat(6) {
+        if (readLine()!!.toString() == "W")
+            count++
+    }
+    val group = when (count) {
+        5, 6 -> {
+            1
+        }
+        3, 4 -> {
+            2
+        }
+        1, 2 -> {
+            3
+        }
+        else -> {
+            -1
+        }
+    }
+    println(group)
+}
