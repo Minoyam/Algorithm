3864daf357035fdacf5e791eff8f292e7cf72ec5
Mino <k3000js@naver.com>
2021-03-25 10:22:34 +0900

    백준 -N13597

---
+++ src/baekjoon/N13597.kt
@@ -0,0 +1,15 @@
+package baekjoon
+
+
+fun main(){
+    readLine()!!.split(" ").map {
+        it.toInt()
+    }.let {
+        if(it.first() == it.last()){
+            print(it.first())
+        }
+        else{
+            print(it.max())
+        }
+    }
+}
