3da5a3baa2c197a76b201752ca989ebbb205ed3e
Mino <k3000js@naver.com>
2021-03-25 10:22:42 +0900

    백준 -N13610

---
+++ src/baekjoon/N13610.kt
@@ -0,0 +1,16 @@
+package baekjoon
+
+fun main() {
+    readLine()!!.split(" ").map {
+        it.toInt()
+    }.let {
+        val a = it.first()
+        var count = 1
+        count += if ((a % (it.last() - it.first()) == 0)) {
+            a / (it.last() - it.first())
+        } else {
+            a / (it.last() - it.first()) + 1
+        }
+        println(count)
+    }
+}
